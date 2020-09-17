37ad1f10427d8927ff8017e60e9e440fa972d3d3
natalianow <56737391+natalianow@users.noreply.github.com>
2020-09-17 23:21:18 +0200

    Add files via upload

---
+++ Ex7.java
@@ -0,0 +1,23 @@
+package NewRepo;
+
+public class Ex7 {
+    public static void main(String[] args) {
+        //int i = -5;
+        /*while (i > 0){
+            System.out.println(i);
+            i--;
+        }
+        System.out.println(i);
+
+         */
+        /*do{
+            System.out.println(i);
+            i--;
+        } while(i > 0);
+
+         */
+        for(int i = 5; i > 0; i--){   // lub int = 5; przed linijka z for, wyrzucamy z nawiasu, wtedy dziala poza petla
+            System.out.println(i);
+        }
+    }
+}
