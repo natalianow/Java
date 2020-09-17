82a168988e8136cb7ef66020cdf987adb63c7d89
natalianow <56737391+natalianow@users.noreply.github.com>
2020-09-17 14:58:02 +0200

    Add files via upload

---
+++ Ex3.java
@@ -0,0 +1,33 @@
+package NewRepo;
+
+public class Ex3 {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+        // int wynik = 0;
+        // a = a + b;
+        /*
+        a += b;
+        a -= b;
+        a *= b;
+        a /= b;
+        a %= b;
+         */
+        //a += 1;
+        // inkrementacja
+        a++;
+        // dekrementacja
+        a--;
+
+
+        System.out.println(Math.pow(9, 3));
+        System.out.println(Math.sqrt(9));
+        System.out.println(Math.abs(-50));
+
+        // postinkrementacja/postdekrementacja
+        System.out.println(a++);
+        System.out.println(a);
+        // preinkrementacja/predekrementacja
+        System.out.println(++a);
+    }
+}
