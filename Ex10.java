58d0e2a1547dfa6c56b41172b39457c6e9bdc51b
natalianow <56737391+natalianow@users.noreply.github.com>
2020-09-24 12:22:23 +0200

    Add files via upload

---
+++ Ex10.java
@@ -0,0 +1,11 @@
+package NewRepo;
+
+public class Ex10 {
+    public static void main(String[] args) {
+        String[] tab = {"Ania", "Bartek", "Adam", "Asia"};
+
+        for(String item : tab){
+            System.out.println(item);
+        }
+    }
+}
