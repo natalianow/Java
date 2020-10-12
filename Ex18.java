9ad4e2e4a2b86a987e5e8f6531c1d744c99d2eca
natalianow <56737391+natalianow@users.noreply.github.com>
2020-10-12 10:45:50 +0200

    Add files via upload

---
+++ Ex18.java
@@ -0,0 +1,19 @@
+package NewRepo;
+
+public class Ex18 {
+    public static void main(String[] args) {
+        // Math
+        int a = 5;
+        int b =11;
+        System.out.println(Math.max(a,b));
+        System.out.println(Math.min(a,b));
+        System.out.println(Math.abs(a));
+        System.out.println(Math.pow(2,3));
+        System.out.println(Math.sqrt(16));
+        System.out.println(Math.round(5.4));
+        System.out.println(Math.ceil(5.1));
+        System.out.println(Math.floor(5.999));
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+    }
+}
